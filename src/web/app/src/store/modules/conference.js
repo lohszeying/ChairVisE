3630580640292c6662d58a057c5b197751508d62
Loh Sze Ying <bloom9334@hotmail.com>
2020-10-19 12:28:46 +0800

    Able to create new conference. NOTE: After clicking on newly created conference, it links to Presentation Details. I have not yet to create version.

--- src/web/app/src/store/modules/conference.js
+++ src/web/app/src/store/modules/conference.js
@@ -1,5 +1,5 @@
-/*import axios from 'axios'
-import {deepCopy} from "@/common/utility"*/
+import axios from 'axios'
+import {deepCopy} from "@/common/utility"
 
 export default {
   state: {
@@ -23,10 +23,78 @@ export default {
       isApiError: false,
       apiErrorMsg: '',
     },
-    isSaveSuccess: false,
-    isPresentationEditable: false
+    isSaveSuccess: false
   },
   mutations: {
+    setConferenceListLoading(state, payload) {
+      if (payload) {
+        state.conferenceListStatus.isApiError = false;
+      }
+      state.conferenceListStatus.isApiError = payload;
+    },
+    setConferenceListApiError(state, payload) {
+      state.conferenceListStatus.isApiError = true;
+      state.conferenceListStatus.apiErrorMsg = payload;
+    },
+    setConferenceList(state, payload) {
+      state.conferenceList = payload;
+    },
+    addToConferenceList(state, payload) {
+      state.conferenceList.push(payload);
+    },
+    deleteFromConferenceList(state, payload) {
+      const index = state.conferenceList.findIndex(conference => conference.id === payload);
+      state.conferenceList.splice(index, 1)
+    },
+
+    //AFTER HERE
+    updateConferenceListWith(state, payload) {
+      state.conferenceList = state.conferenceList.map(conference => {
+        if (conference.id === payload.id) {
+          return payload
+        }
+        return conference
+      });
+    },
+
+    setConferenceFormLoading(state, isLoading) {
+      state.conferenceFormStatus.isLoading = isLoading;
+    },
+
+    setConferenceFormApiError(state, msg) {
+      state.conferenceFormStatus.isApiError = true;
+      state.conferenceFormStatus.apiErrorMsg = msg;
+    },
+
+    setConferenceForm(state, payload) {
+      state.conferenceForm = payload;
+    },
+
+    resetConferenceForm(state) {
+      state.conferenceForm.id = '';
+      state.conferenceForm.name = '';
+      state.conferenceForm.version = '';
+      state.conferenceForm.description = '';
+      state.conferenceForm.creatorIdentifier = '';
+      state.conferenceFormStatus.isLoading = false;
+      state.conferenceFormStatus.isApiError = false;
+      state.conferenceFormStatus.apiErrorMsg = '';
+    },
+
+    setConferenceFormField(state, {field, value}) {
+      state.conferenceForm[field] = value
+    },
+
+    setSaveSuccess(state, success) {
+      state.isSaveSuccess = success;
+    }
+    /*
+    setIsConferenceEditable(state, isPresentationEditable) {
+      //May not need to use this
+      state.isPresentationEditable = isPresentationEditable;
+    }*/
+
+    //Originally, this was in presentation
     /*setConferenceListLoading(state, payload) {
       if (payload) {
         state.presentationListStatus.isApiError = false;
@@ -102,6 +170,80 @@ export default {
     } */
   },
   actions: {
+    async getConferenceList({commit}) {
+      commit('setConferenceListLoading', true);
+      axios.get('/api/conferences')
+          .then(response => {
+            commit('setConferenceList', response.data)
+          })
+          .catch(e => {
+            commit('setConferenceListApiError', e.toString());
+          })
+          .finally(() => {
+            commit('setConferenceListLoading', false);
+          })
+    },
+    async getConference({commit}, conferenceId) {
+
+      commit('setConferenceFormLoading', true);
+      await axios.get(`/api/conferences/${conferenceId}`)
+          .then(response => {
+            commit('setConferenceForm', response.data)
+          })
+          .catch(e => {
+            commit('setConferenceFormApiError', e.toString());
+          })
+          .finally(() => {
+            commit('setConferenceFormLoading', false);
+          });
+    },
+
+    async saveConference({commit, state}) {
+      commit('setConferenceFormLoading', true);
+      await axios.post('/api/conferences', state.presentationForm)
+          .then(response => {
+            commit('addToConferenceList', deepCopy(response.data));
+            commit('setConferenceForm', deepCopy(response.data));
+            commit("setSaveSuccess", true);
+          })
+          .catch(e => {
+            commit('setConferenceFormApiError', e.toString());
+            commit("setSaveSuccess", false);
+          })
+          .finally(() => {
+            commit('setConferenceFormLoading', false);
+          })
+    },
+
+    async updateConference({commit, state}) {
+      commit('setConferenceFormLoading', true);
+      await axios.put('/api/conferences/' + state.presentationForm.id, state.presentationForm)
+          .then(response => {
+            commit('updateConferenceListWith', response.data)
+          })
+          .catch(e => {
+            commit('setConferenceFormApiError', e.toString());
+          })
+          .finally(() => {
+            commit('setConferenceFormLoading', false);
+          })
+    },
+
+    async deleteConference({commit}, payload) {
+      commit('setConferenceFormLoading', true);
+      await axios.delete('/api/conferences/' + payload)
+          .then(() => {
+            commit('deleteFromConferenceList', payload);
+            commit('resetConferenceForm')
+          })
+          .catch(e => {
+            commit('setConferenceFormApiError', e.toString());
+          })
+          .finally(() => {
+            commit('setConferenceFormLoading', false);
+          })
+    }
+
     /*async getConferenceList({commit}) {
       commit('setPresentationListLoading', true);
       axios.get('/api/presentations')
--- src/web/app/src/views/NewConference.vue
+++ src/web/app/src/views/NewConference.vue
@@ -2,12 +2,13 @@
     <el-main>
         <el-card>
             <div slot="header" class="clearfix">
-                <span> Add New Calendar  </span>
+                <span> Create New Conference </span>
             </div>
             <el-alert v-if="isNewConference && !isLogin" title="Please login to create new conference" type="error" show-icon
                       class="errorMsg"/>
             <el-form v-else :rules="rules" ref="conferenceForm"
                      :model="conferenceForm" v-loading="isLoading">
+
                 <el-form-item label="Name" :prop="'name'" >
                     <el-col>
                         <el-input v-model="conferenceFormName" placeholder="Enter name"/>
@@ -15,25 +16,14 @@
                 </el-form-item>
                 <el-form-item label="Description">
                     <el-col>
-                        <el-input v-model="conferenceFormDescription" placeholder="Enter description"/>
+                        <el-input type="textarea" v-model="conferenceFormDescription" placeholder="Enter description"/>
                     </el-col>
                 </el-form-item>
-                <el-form-item label="Conference Date" :prop="'date'">
-                    <el-col>
-                        <el-date-picker
-                                v-model="conferenceFormDate"
-                                type="datetime"
-                                placeholder="Select date and time">
-                        </el-date-picker>
-                    </el-col>
-                </el-form-item>
-
                 <el-form-item>
                     <el-button type="primary" icon="el-icon-check" @click="uploadClicked()">Save</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
-
 
         <!-- dialogs -->
         <el-dialog
@@ -60,10 +50,10 @@
 </template>
 
 <script>
-    import Datepicker from 'vuejs-datepicker';
-    import {ID_NEW_CONFERENCE} from "../common/const";
+    import {AccessLevel, ID_NEW_CONFERENCE, SPECIAL_IDENTIFIER_PUBLIC} from "@/common/const";
+
     export default {
-        name: "AddConference",
+        name: 'ConferenceBrief',
         props: {
             id: String
         },
@@ -74,6 +64,7 @@
         },
         mounted() {
             this.updateConferenceForm();
+            //this.$store.dispatch('getVersionList')
         },
         computed: {
             isLogin() {
@@ -82,9 +73,12 @@
             conferenceForm() {
                 return {
                     name: this.conferenceFormName,
+                    creatorIdentifier: this.conferenceFormCreatorIdentifier,
                     description: this.conferenceFormDescription,
-                    date: this.conferenceFormDate,
                 }
+            },
+            conferenceFormCreatorIdentifier() {
+                return this.$store.state.conference.conferenceForm.creatorIdentifier
             },
             conferenceFormName: {
                 get() {
@@ -108,17 +102,7 @@
                     })
                 },
             },
-            conferenceFormDate: {
-                get() {
-                    return this.$store.state.conference.conferenceForm.date
-                },
-                set(value) {
-                    this.$store.commit('setConferenceFormField', {
-                        field: 'date',
-                        value
-                    })
-                },
-            },
+
             isNewConference() {
                 return this.id === ID_NEW_CONFERENCE
             },
@@ -138,35 +122,58 @@
                 return this.$store.state.conference.conferenceFormStatus.apiErrorMsg
             }
         },
+        data() {
+            return {
+                hasSubmitted: false,
+                rules: {
+                    name: [
+                        {required: true, message: 'Please enter conference name', trigger: 'blur'},
+                        {min: 3, message: 'The length should be more than 3 character', trigger: 'blur'}
+                    ],
+                }
+            }
+        },
         methods: {
             addConference() {
                 this.hasSubmitted = false;
                 this.$store.dispatch('saveConference').then(() => {
-                        if (this.isNewConference && !this.isLogin) {
-                            return
-                        }
-                    });
+                    if (this.isNewConference && !this.isLogin) {
+                        return
+                    }
+                });
             },
             updateConferenceForm() {
                 if (this.$refs['conferenceForm']) {
                     this.$refs['conferenceForm'].clearValidate();
                 }
-                this.$store.commit('resetConferenceForm');
+                this.$store.commit('resetConferenceFormForm');
+                if (this.id !== ID_NEW_CONFERENCE) {
+                    this.$store.dispatch('getConference', this.id)
+                        .then(() => {
+                            // check writable or not
+                            this.$store.dispatch('fetchAccessControlList', this.id)
+                                .then(() => {
+                                    let currentUser = this.$store.state.userInfo.userEmail;
+                                    let accessControlList = this.$store.state.accessControl.accessControlList;
+                                    let isPresentationEditable =
+                                        currentUser === this.conferenceFormCreatorIdentifier
+                                        || accessControlList.some(acl => acl.userIdentifier === currentUser && acl.permission === AccessLevel.CAN_WRITE)
+                                        || accessControlList.some(acl => acl.userIdentifier === SPECIAL_IDENTIFIER_PUBLIC && acl.permission === AccessLevel.CAN_WRITE);
+                                    this.$store.commit('setIsPresentationEditable', isPresentationEditable)
+                                })
+                        })
+                } else {
+                    this.$store.dispatch("getVersionList")
+                }
             },
             uploadClicked() {
                 this.$refs['conferenceForm'].validate((valid, object) => {
+                    console.log("save clicked");
                     if (!valid) {
                         if('name' in object) {
                             this.$notify.error({
                                 title: 'Error',
                                 message: object.name[0].message
-                            });
-                        }
-                        if ('date' in object) {
-                            this.$notify.error({
-                                title: 'Error',
-                                message: object.date[0].message,
-                                offset: 100
                             });
                         }
                         return
@@ -178,30 +185,17 @@
             closeSuccess() {
                 this.$store.commit("setSaveSuccess", false);
                 this.$router.push({
-                    name: 'conference'
+                    name: 'manage'
                 });
             }
         },
         components: {
-            Datepicker,
-        },
-        data() {
-            return {
-                hasSubmitted: false,
-                rules: {
-                    name: [
-                        {required: true, message: 'Please enter conference name', trigger: 'blur'},
-                        {min: 3, message: 'The length should be more than 3 character', trigger: 'blur'}
-                    ],
-                    date: [
-                        {required: true, message: 'Please select a conference date and time.', trigger: 'blur'},
-                    ]
-                }
-            }
         },
     }
 </script>
 
 <style scoped>
-
+    .errorMsg {
+        margin-bottom: 18px;
+    }
 </style>
