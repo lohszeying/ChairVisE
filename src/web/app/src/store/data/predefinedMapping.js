f225ad113432dadaf6699b9f9e75326ab71a1bd9
Loh Sze Ying <bloom9334@hotmail.com>
2020-11-02 19:16:08 +0800

    added author to pre-defined mapping for submission record

--- src/web/app/src/store/data/predefinedMapping.js
+++ src/web/app/src/store/data/predefinedMapping.js
@@ -28,7 +28,7 @@ export default [
     name: "Default Submission Mapping",
     mapping: {
       dbTagIndices: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-      importedTagIndices: [0, 1, 2, 3, 5, 6, 8, 9, 10, 11, 12, 13, 14]
+      importedTagIndices: [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13]
     }
   }
 ]
